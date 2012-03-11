c0cff9e34934c371d2b7b74e66f103b5800c105b
suchanek <suchanek@mpi-inf.mpg.de>
2012-03-11 12:50:07 +0000

    Added callNow parameter

--- src/main/Caller.java
+++ src/main/Caller.java
@@ -34,11 +34,20 @@ public class Caller {
 	/** Header for the YAGO files */
 	public static String header = "This file is part of the ontology YAGO2s\nIt is licensed under a Creative-Commons Attribution License by the YAGO team\nat the Max Planck Institute for Informatics/Germany.\nSee http://yago-knowledge.org for all details.\n\n";
 
+	/** Calls specific extractors in the given order */
+	public static void callNow(List<Extractor> extractors) throws Exception {
+		Announce.doing("Calling extractors");
+		for (Extractor e : extractors) {
+			e.extract(outputFolder, header);
+		}
+		Announce.done();
+	}
+
 	/** Calls all extractors in the right order */
 	public static void call(List<Extractor> extractors) throws Exception {
 		Set<Theme> themesWeHave = new TreeSet<Theme>();
 		Announce.doing("Calling extractors");
-		Announce.message("Extractors",extractors);
+		Announce.message("Extractors", extractors);
 		for (int i = 0; i < extractors.size(); i++) {
 			Extractor e = extractors.get(i);
 			if (e.input().isEmpty() || themesWeHave.containsAll(e.input())) {
@@ -47,23 +56,25 @@ public class Caller {
 				extractors.remove(i);
 				Announce.message("----------------------------");
 				Announce.message("Current themes:", themesWeHave);
-				Announce.message("Current extractors:",extractors);
+				Announce.message("Current extractors:", extractors);
 				i = -1; // Start again from the beginning
 			}
 		}
 		// Call the ALL extractors
-		for(int i = 0; i < extractors.size(); i++) {
+		for (int i = 0; i < extractors.size(); i++) {
 			Extractor e = extractors.get(i);
-			if(!e.input().contains(Theme.ALL)) continue;
+			if (!e.input().contains(Theme.ALL))
+				continue;
 			e.extract(outputFolder, header);
 			themesWeHave.addAll(e.output().keySet());
 			extractors.remove(i);
 			Announce.message("----------------------------");
 			Announce.message("Current themes:", themesWeHave);
-			Announce.message("Current extractors:",extractors);
+			Announce.message("Current extractors:", extractors);
 			i--;
 		}
-		if(!extractors.isEmpty()) Announce.warning("Could not call",extractors);
+		if (!extractors.isEmpty())
+			Announce.warning("Could not call", extractors);
 		Announce.done();
 	}
 
@@ -71,17 +82,16 @@ public class Caller {
 	public static List<Extractor> extractors(List<String> extractorNames) {
 		Announce.doing("Creating extractors");
 		if (extractorNames == null) {
-			Announce
-					.error("No extractors given\nThe ini file should contain:\nextractors = extractorClass(fileName), ...");
+			Announce.error("No extractors given\nThe ini file should contain:\nextractors = extractorClass(fileName), ...");
 		}
 		if (extractorNames.isEmpty()) {
-			Announce
-					.error("Empty extractor list\nThe ini file should contain:\nextractors = extractorClass(fileName), ...");
+			Announce.error("Empty extractor list\nThe ini file should contain:\nextractors = extractorClass(fileName), ...");
 		}
 		List<Extractor> extractors = new ArrayList<Extractor>();
 		for (String extractorName : extractorNames) {
-			Extractor e=extractorForCall(extractorName);
-			if(e!=null) extractors.add(e);
+			Extractor e = extractorForCall(extractorName);
+			if (e != null)
+				extractors.add(e);
 		}
 		Announce.done();
 		return (extractors);
@@ -94,31 +104,36 @@ public class Caller {
 		if (!m.matches()) {
 			Announce.error("Cannot understand extractor call:", extractorName);
 			Announce.failed();
-			return(null);
+			return (null);
 		}
-		Extractor extractor = Extractor.forName(m.group(1), m.group(2) == null || m.group(2).isEmpty() ? null : new File(m.group(2)));
+		Extractor extractor = Extractor.forName(m.group(1), m.group(2) == null || m.group(2).isEmpty() ? null
+				: new File(m.group(2)));
 		if (extractor == null) {
 			Announce.failed();
-			return(null);
+			return (null);
 		}
 		Announce.done();
-		return(extractor);
+		return (extractor);
 	}
-	
+
 	/** Run */
 	public static void main(String[] args) throws Exception {
 		Announce.doing("Creating YAGO");
-		long time=System.currentTimeMillis();
-		Announce.message("Starting at",NumberFormatter.ISOtime());
+		long time = System.currentTimeMillis();
+		Announce.message("Starting at", NumberFormatter.ISOtime());
 		String initFile = args.length == 0 ? "yago.ini" : args[0];
 		Announce.doing("Initializing from", initFile);
 		Parameters.init(initFile);
 		Announce.done();
 		outputFolder = Parameters.getOrRequestAndAddFile("yagoFolder", "the folder where YAGO should be created");
-		call(extractors(Parameters.getList("extractors")));
-		long now=System.currentTimeMillis();
-		Announce.message("Finished at",NumberFormatter.ISOtime());
-		Announce.message("Time needed:",NumberFormatter.formatMS(now-time));
+		if (Parameters.isDefined("callNow")) {
+			call(extractors(Parameters.getList("callNow")));
+		} else {
+			call(extractors(Parameters.getList("extractors")));
+		}
+		long now = System.currentTimeMillis();
+		Announce.message("Finished at", NumberFormatter.ISOtime());
+		Announce.message("Time needed:", NumberFormatter.formatMS(now - time));
 		Announce.done();
 	}
 }
