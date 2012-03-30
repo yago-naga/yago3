5a09a4f8e536228b63addafa92f560d5bc414959
suchanek <suchanek@mpi-inf.mpg.de>
2012-03-30 15:21:08 +0000

    minor

--- scripts/db.sh
+++ scripts/db.sh
@@ -4,7 +4,7 @@ if [ `hostname` != "d5blade05" ]; then
   exit
 fi
 echo Loading YAGO into the database
-cd /local/users/yago2s
+cd /local/suchanek/yago2s
 echo yago2itnyago | psql -a -d yago2s -h postgres0 -U yago -f ~/workspace/converters2s/scripts/postgres.sql > ~/workspace/db.log &
 disown -h %1
 sleep 5s
