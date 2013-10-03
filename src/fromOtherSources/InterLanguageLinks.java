6d0b366e0805b58958e3ed33ce607b791de70cde
fmahdiso <fmahdiso@mpi-inf.mpg.de>
2013-10-03 16:03:12 +0000

    InterLanguageLinks Extractor added in the package fromOtherSources.

---
+++ src/fromOtherSources/InterLanguageLinks.java
@@ -0,0 +1,89 @@
+package fromOtherSources;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import fromWikipedia.Extractor;
+import javatools.administrative.Announce;
+import javatools.datatypes.FinalSet;
+import javatools.parsers.Char;
+import javatools.util.FileUtils;
+import basics.Fact;
+import basics.FactComponent;
+import basics.FactSource;
+import basics.FactWriter;
+import basics.N4Reader;
+import basics.Theme;
+
+public class InterLanguageLinks extends Extractor {
+
+	private File inputFile; 
+	public static final Theme INTERLANGUAGELINKS=new Theme("yagoInterLanguageLinks","The manually created inter language synonyms");
+	
+	public InterLanguageLinks(File inputFolder) {
+		this.inputFile = inputFolder.isFile()?inputFolder:new File(inputFolder,"wikidata.rdf"); 
+	}
+	
+	public Set<Theme> output() {
+		return (new FinalSet<Theme>(INTERLANGUAGELINKS));
+	}
+	
+	public void extract(File input, FactWriter writer) throws Exception {
+		Announce.doing("", input.getName());
+//		"C://Users//Administrator//Downloads//turtle-20130808-links.ttl"
+		N4Reader nr = new N4Reader(FileUtils.getBufferedUTF8Reader(input));
+		Map<String,String> correspondence =  new HashMap<String, String>();
+		while(nr.hasNext()){
+			Fact f=nr.next();
+			if(f.getArg(2).contains("#Item")){
+				String englishName= correspondence.get("en");
+				correspondence.remove("en");
+				if(englishName !=null){
+					englishName = Char.cutLast(englishName);
+					for (Map.Entry<String, String> entry : correspondence.entrySet())
+					{
+						writer.write(new Fact(FactComponent.forYagoEntity(Char.decodePercentage(englishName)),  "rdfs:label"
+								, FactComponent.forStringWithLanguage(Char.decodePercentage(Char.cutLast(entry.getValue())), entry.getKey())));
+					}
+				}
+				correspondence.clear();
+				
+				f= nr.next();
+			}else if (f.getRelation().contains("Language")){
+				String[] parts = f.getArg(1).split("/");
+				String name = parts[parts.length - 1 ];
+				correspondence.put(FactComponent.stripQuotes(f.getArg(2)), name);
+			}
+		
+		}
+		nr.close();
+	}
+	
+	@Override
+	public void extract(Map<Theme, FactWriter> writers, Map<Theme, FactSource> factCollections) throws Exception {
+		Announce.doing("Copying patterns");
+		Announce.message("Input folder is", inputFile);
+			extract(inputFile, writers.get(INTERLANGUAGELINKS));
+		Announce.done();
+	}
+
+
+	public static void main(String[] args) {
+		try {
+			new InterLanguageLinks(new File("C://Users//Administrator//data//wikidata.rdf"))
+			.extract(new File("C:/Users/Administrator/data2/yago2s"), "test");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Set<Theme> input() {
+	 return Collections.emptySet();
+	}
+
+
+}
