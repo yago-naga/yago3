46d8a25538bdff2807dfc6e34577725fa37591a9
fmahdiso <fmahdiso@mpi-inf.mpg.de>
2013-10-18 08:22:09 +0000

    This class is made abstract. InfoboxMappers for different languages will be its descendants.

--- src/fromWikipedia/InfoboxMapper.java
+++ src/fromWikipedia/InfoboxMapper.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
