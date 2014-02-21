e916e5b74ca278c9720c83d68d5698f3a6280d24
jbiega <jbiega@mpi-inf.mpg.de>
2014-02-21 10:55:54 +0000

    Fix the translator so that the brackets are not stripped for literals. Cleanup the indentation.

--- src/fromWikipedia/Translator.java
+++ src/fromWikipedia/Translator.java
@@ -27,115 +27,157 @@ import basics.Theme;
 
 public class Translator extends FollowUpExtractor {
 
-  private String language;
+	private String language;
 
-  private String objectType;
+	private String objectType;
 
-  @Override
-  public Set<Theme> input() {
-    return new HashSet<Theme>(Arrays.asList(checkMe, InterLanguageLinks.INTERLANGUAGELINKS));
-  }
-
-  @Override
-  public Set<Theme> output() {
-    return new FinalSet<Theme>(checked);
-  }
-
-  @Override
-  public void extract(Map<Theme, FactWriter> output, Map<Theme, FactSource> input) throws Exception {
-
-    Map<String, String> tempDictionary = InterLanguageLinksDictionary.get(language, input.get(InterLanguageLinks.INTERLANGUAGELINKS));
-
-//    FactCollection translatedFacts = new FactCollection();
-
-//    FactCollection temp = new FactCollection();
-//    loadFacts(input.get(checkMe), temp);
-
-    for (Fact f : input.get(checkMe)) {
-
-      /*If subject or object is not found in the dictionary, it remains the same as itself.
-       * All the subjects are assumed to be entities. */
-
-      String translatedSubject = FactComponent.stripBrackets(f.getArg(1));
-      String translatedObject = FactComponent.stripBrackets(f.getArg(2));
-
-      if (tempDictionary.containsKey(translatedSubject)) {
-        translatedSubject = tempDictionary.get(translatedSubject);
-      }
-      if (objectType.equals("Entity")) {
-
-	//For non-literals translate the entity and put back into yagoEntity form
-	if (!FactComponent.isLiteral(translatedObject)) {
-	  if (tempDictionary.containsKey(translatedObject)) {
-	    translatedObject = tempDictionary.get(translatedObject);
-	  }
-	  translatedObject = FactComponent.forUri(translatedObject);
+	@Override
+	public Set<Theme> input() {
+		return new HashSet<Theme>(Arrays.asList(checkMe,
+				InterLanguageLinks.INTERLANGUAGELINKS));
 	}
 
-        output.get(checked).write(new Fact(FactComponent.forYagoEntity(translatedSubject), f.getRelation(), translatedObject));
-//        translatedFacts.add(new Fact(FactComponent.forYagoEntity(translatedSubject), f.getRelation(), FactComponent.forString(translatedObject)));
+	@Override
+	public Set<Theme> output() {
+		return new FinalSet<Theme>(checked);
+	}
 
-      } else if (objectType.equals("Infobox")) {
+	@Override
+	public void extract(Map<Theme, FactWriter> output,
+			Map<Theme, FactSource> input) throws Exception {
 
-        String infoboxWord = InterLanguageLinksDictionary.getInfDictionary(input.get(InterLanguageLinks.INTERLANGUAGELINKS)).get(language);
+		Map<String, String> tempDictionary = InterLanguageLinksDictionary.get(
+				language, input.get(InterLanguageLinks.INTERLANGUAGELINKS));
 
-        String word = FactComponent.stripBrackets(f.getArg(2).replace(" ", "_"));
-        if (word.length() > 0) word = word.substring(0, 1).toUpperCase() + word.substring(1);
+		// FactCollection translatedFacts = new FactCollection();
 
-        translatedObject = infoboxWord + "_" + word;
+		// FactCollection temp = new FactCollection();
+		// loadFacts(input.get(checkMe), temp);
 
-        if (tempDictionary.containsKey(translatedObject)) translatedObject = tempDictionary.get(translatedObject);
+		for (Fact f : input.get(checkMe)) {
 
-        if (translatedObject.contains("_")) {
-          translatedObject = translatedObject.substring(translatedObject.lastIndexOf("_") + 1);
-        }
+			/*
+			 * If subject or object is not found in the dictionary, it remains
+			 * the same as itself. All the subjects are assumed to be entities.
+			 */
 
-        output.get(checked).write(new Fact(FactComponent.forYagoEntity(translatedSubject), "<hasInfoboxType/en>", FactComponent.forString(translatedObject)));
-//        translatedFacts
-//            .add(new Fact(FactComponent.forYagoEntity(translatedSubject), "<hasInfoboxType/en>", FactComponent.forString(translatedObject)));
-        
+			String translatedSubject = FactComponent.stripBrackets(f.getArg(1));
+			String translatedObject = f.getArg(2);
+					
+			if (tempDictionary.containsKey(translatedSubject)) {
+				translatedSubject = tempDictionary.get(translatedSubject);
+			}
+			
+			if (objectType.equals("Entity")) {
 
-        
-      } else if (objectType.equals("Category")) {
+				// For non-literals translate the entity and put back into
+				// yagoEntity form
+				if (!FactComponent.isLiteral(translatedObject)) {
+					
+					translatedObject = FactComponent.stripBrackets(translatedObject);
+					
+					if (tempDictionary.containsKey(translatedObject)) {
+						translatedObject = tempDictionary.get(translatedObject);
+					}
+					translatedObject = FactComponent.forUri(translatedObject);
+				}
 
-        String categoryWord = InterLanguageLinksDictionary.getCatDictionary(input.get(InterLanguageLinks.INTERLANGUAGELINKS)).get(language); 
-        translatedObject = categoryWord + ":" +FactComponent.stripQuotes(f.getArg(2).replace(" ", "_"));
-        if (tempDictionary.containsKey(translatedObject)) translatedObject = tempDictionary.get(translatedObject);
+				output.get(checked).write(
+						new Fact(
+								FactComponent.forYagoEntity(translatedSubject),
+								f.getRelation(), translatedObject));
+				// translatedFacts.add(new
+				// Fact(FactComponent.forYagoEntity(translatedSubject),
+				// f.getRelation(), FactComponent.forString(translatedObject)));
 
-        if (translatedObject.contains(":")) {
-          translatedObject = translatedObject.substring(translatedObject.lastIndexOf(":") + 1);
-        }
+			} else if (objectType.equals("Infobox")) {
 
-        output.get(checked).write(new Fact(FactComponent.forYagoEntity(translatedSubject), "<hasWikiCategory/en>", FactComponent
-            .forString(translatedObject)));
-//        translatedFacts.add(new Fact(FactComponent.forYagoEntity(translatedSubject), "<hasWikiCategory/en>", FactComponent
-//            .forString(translatedObject)));
+				String infoboxWord = InterLanguageLinksDictionary
+						.getInfDictionary(
+								input.get(InterLanguageLinks.INTERLANGUAGELINKS))
+						.get(language);
 
-      }
+				String word = FactComponent.stripBrackets(f.getArg(2).replace(
+						" ", "_"));
+				if (word.length() > 0)
+					word = word.substring(0, 1).toUpperCase()
+							+ word.substring(1);
 
-    }
-//    for (Fact fact : translatedFacts) {
-//      output.get(checked).write(fact);
-//    }
-  }
+				translatedObject = infoboxWord + "_" + word;
 
-  public Translator(Theme in, Theme out, String lang, String objectType) {
-    this.checkMe = in;
-    this.checked = out;
-    this.language = lang;
-    this.objectType = objectType;
-  }
+				if (tempDictionary.containsKey(translatedObject))
+					translatedObject = tempDictionary.get(translatedObject);
 
-  protected FactCollection loadFacts(FactSource factSource, FactCollection temp) {
-    for (Fact f : factSource) {
-      temp.add(f);
-    }
-    return (temp);
-  }
+				if (translatedObject.contains("_")) {
+					translatedObject = translatedObject
+							.substring(translatedObject.lastIndexOf("_") + 1);
+				}
 
-  public static void main(String[] args) throws Exception {
-    Theme res = new Theme("res", "" );
-    new Translator(CategoryExtractor.CATEGORYMEMBERS_MAP.get("de"), res, "de", "Category").extract(new File("D:/data3/yago2s"), "");
-  }
+				output.get(checked).write(
+						new Fact(
+								FactComponent.forYagoEntity(translatedSubject),
+								"<hasInfoboxType/en>", FactComponent
+										.forString(translatedObject)));
+				// translatedFacts
+				// .add(new Fact(FactComponent.forYagoEntity(translatedSubject),
+				// "<hasInfoboxType/en>",
+				// FactComponent.forString(translatedObject)));
+
+			} else if (objectType.equals("Category")) {
+
+				String categoryWord = InterLanguageLinksDictionary
+						.getCatDictionary(
+								input.get(InterLanguageLinks.INTERLANGUAGELINKS))
+						.get(language);
+				translatedObject = categoryWord
+						+ ":"
+						+ FactComponent.stripQuotes(f.getArg(2).replace(" ",
+								"_"));
+				if (tempDictionary.containsKey(translatedObject))
+					translatedObject = tempDictionary.get(translatedObject);
+
+				if (translatedObject.contains(":")) {
+					translatedObject = translatedObject
+							.substring(translatedObject.lastIndexOf(":") + 1);
+				}
+
+				output.get(checked).write(
+						new Fact(
+								FactComponent.forYagoEntity(translatedSubject),
+								"<hasWikiCategory/en>", FactComponent
+										.forString(translatedObject)));
+				// translatedFacts.add(new
+				// Fact(FactComponent.forYagoEntity(translatedSubject),
+				// "<hasWikiCategory/en>", FactComponent
+				// .forString(translatedObject)));
+
+			}
+
+		}
+		// for (Fact fact : translatedFacts) {
+		// output.get(checked).write(fact);
+		// }
+	}
+
+	public Translator(Theme in, Theme out, String lang, String objectType) {
+		this.checkMe = in;
+		this.checked = out;
+		this.language = lang;
+		this.objectType = objectType;
+	}
+
+	protected FactCollection loadFacts(FactSource factSource,
+			FactCollection temp) {
+		for (Fact f : factSource) {
+			temp.add(f);
+		}
+		return (temp);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Theme res = new Theme("res", "");
+		new Translator(CategoryExtractor.CATEGORYMEMBERS_MAP.get("de"), res,
+				"de", "Category").extract(new File("D:/data3/yago2s"), "");
+	}
 
 }
