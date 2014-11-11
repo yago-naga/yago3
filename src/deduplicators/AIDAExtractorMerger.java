b757bfee79e64cfb738feb876145e192eb36225f
mamir <mamir@mpi-inf.mpg.de>
2014-11-11 10:51:00 +0000

    splitting AIDA extractors into two, one for functional relations, and the other for non-functional relation. This should save the time/memory for unnecessarily checking functional constrains.

---
+++ src/deduplicators/AIDAExtractorMerger.java
@@ -0,0 +1,92 @@
+package deduplicators;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javatools.administrative.Announce;
+import javatools.datatypes.FinalSet;
+import utils.Theme;
+import utils.Theme.ThemeGroup;
+import basics.Fact;
+import basics.RDFS;
+import extractors.Extractor;
+import extractors.MultilingualExtractor;
+import fromOtherSources.HardExtractor;
+import fromThemes.TransitiveTypeExtractor;
+import fromWikipedia.CategoryExtractor;
+import fromWikipedia.ConteXtExtractor;
+import fromWikipedia.DisambiguationPageExtractor;
+import fromWikipedia.RedirectExtractor;
+import fromWikipedia.StructureExtractor;
+
+
+public class AIDAExtractorMerger extends Extractor {
+
+  /** All facts of YAGO */
+  public static final Theme AIDAFACTS = new Theme("aidaFacts",
+      "All facts necessary for AIDA", ThemeGroup.OTHER);
+  
+  /** Relations that AIDA needs. */
+  public static final Set<String> relations = new FinalSet<>(
+      RDFS.type, RDFS.subclassOf, RDFS.label, "skos:prefLabel",
+      "<isPreferredMeaningOf>", "<hasGivenName>", "<hasFamilyName>",
+      "<hasGender>", "<hasAnchorText>", "<hasInternalWikipediaLinkTo>",
+      "<redirectedFrom>", "<hasWikipediaUrl>", "<hasCitationTitle>",
+      "<hasWikipediaCategory>", "<hasWikipediaAnchorText>");
+
+  
+  @Override
+  public Set<Theme> input() {
+    Set<Theme> input = new HashSet<Theme>();
+    
+    //YAGO functional facts needed for AIDA
+    //hasWIkipediaUrl, isPrefferredMeaningOf, skof:prefLabel, hasGender
+    //hasGivenName, hasFamilyName
+    input.add(AIDAFunctionalExtractor.AIDAFUNCTIONALFACTS);
+    
+    //the rest of the facts that don't need functional check
+    // Dictionary.
+    input.addAll(StructureExtractor.STRUCTUREFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages)); // also gives links and anchor texts.
+    input.addAll(DisambiguationPageExtractor.DISAMBIGUATIONMEANSFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    input.addAll(RedirectExtractor.REDIRECTFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    input.add(HardExtractor.HARDWIREDFACTS);
+    
+    // Types and Taxonomy.
+    input.add(TransitiveTypeExtractor.TRANSITIVETYPE);
+    input.add(ClassExtractor.YAGOTAXONOMY);
+    
+    // Keyphrases.
+    input.addAll(ConteXtExtractor.CONTEXTFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    input.addAll(CategoryExtractor.CATEGORYMEMBERS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    return input;
+  }
+
+  @Override
+  public Set<Theme> output() {
+    return new FinalSet<>(AIDAFACTS);
+  }
+
+  @Override
+  public void extract() throws Exception {
+    Announce.doing("Merging all AIDA Sources");
+    for (Theme theme : input()) {
+      Announce.doing("Merging facts from", theme);
+      for (Fact fact : theme) {
+        if(isAIDARelation(fact)) {
+          AIDAFACTS.write(fact);          
+        }
+      }
+      Announce.done();
+    }
+    Announce.done();
+  }
+  
+  public boolean isAIDARelation(Fact fact) {
+    if (relations.contains(fact.getRelation())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
---
+++ src/deduplicators/AIDAFunctionalExtractor.java
@@ -0,0 +1,78 @@
+package deduplicators;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javatools.administrative.Announce;
+import utils.Theme;
+import utils.Theme.ThemeGroup;
+import basics.Fact;
+import fromOtherSources.HardExtractor;
+import fromOtherSources.WikidataLabelExtractor;
+import fromThemes.PersonNameExtractor;
+import fromWikipedia.GenderExtractor;
+import fromWikipedia.WikiInfoExtractor;
+
+/**
+ * AIDA Fact Extractor
+ * 
+ * Extracts all facts necessary for AIDA and puts them in a single file.
+ */
+public class AIDAFunctionalExtractor extends SimpleDeduplicator {
+
+	@Override
+	@ImplementationNote("Hardwired facts go first. Infoboxes should go before categories")
+	public List<Theme> inputOrdered() {
+		List<Theme> input = new ArrayList<Theme>();
+		
+		// For YAGO compliance.
+		input.add(SchemaExtractor.YAGOSCHEMA);
+		
+		// Dictionary.
+		input.add(PersonNameExtractor.PERSONNAMES);
+		input.add(PersonNameExtractor.PERSONNAMEHEURISTICS);
+		input.add(GenderExtractor.PERSONS_GENDER);
+		input.add(WikidataLabelExtractor.WIKIPEDIALABELS);
+		input.add(WikidataLabelExtractor.WIKIDATAMULTILABELS);
+		input.add(HardExtractor.HARDWIREDFACTS);
+		
+		// Metadata.
+		input.add(WikiInfoExtractor.WIKIINFO);
+		
+		return input;
+	}
+
+	/** All facts of YAGO */
+	public static final Theme AIDAFUNCTIONALFACTS = new Theme("aidaFunctionalFacts",
+			"All functional facts necessary for AIDA", ThemeGroup.OTHER);
+
+	/** All facts of YAGO */
+	public static final Theme AIDAFUNCTIONALCONFLICTS = new Theme("_aidaFunctionalFactConflicts",
+			"Facts that were not added because they conflicted with an existing fact");
+
+	@Override
+	public Theme myOutput() {
+		return AIDAFUNCTIONALFACTS;
+	}
+
+	@Override
+	public Theme conflicts() {
+		return AIDAFUNCTIONALCONFLICTS;
+	}
+
+	@Override
+	public boolean isMyRelation(Fact fact) {
+		if (AIDAExtractorMerger.relations.contains(fact.getRelation())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Announce.setLevel(Announce.Level.DEBUG);
+		new AIDAFunctionalExtractor().extract(new File("C:/fabian/data/yago3"), "test");
+	}
+
+}
