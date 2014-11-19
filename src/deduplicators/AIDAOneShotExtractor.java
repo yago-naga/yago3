24e23cbe4d00c1d7c93d6442ef4e2dbe3afc50cb
mamir <mamir@mpi-inf.mpg.de>
2014-11-19 13:08:06 +0000

    Adding back AIDAExtractor to extract all relations in one shot.  Will be used for comparison only.

---
+++ src/deduplicators/AIDAOneShotExtractor.java
@@ -0,0 +1,106 @@
+package deduplicators;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javatools.administrative.Announce;
+import javatools.datatypes.FinalSet;
+import utils.Theme;
+import utils.Theme.ThemeGroup;
+import basics.Fact;
+import basics.RDFS;
+import extractors.MultilingualExtractor;
+import fromOtherSources.HardExtractor;
+import fromOtherSources.WikidataLabelExtractor;
+import fromThemes.PersonNameExtractor;
+import fromThemes.TransitiveTypeExtractor;
+import fromWikipedia.CategoryExtractor;
+import fromWikipedia.ConteXtExtractor;
+import fromWikipedia.DisambiguationPageExtractor;
+import fromWikipedia.GenderExtractor;
+import fromWikipedia.RedirectExtractor;
+import fromWikipedia.StructureExtractor;
+import fromWikipedia.WikiInfoExtractor;
+
+/**
+ * AIDA Fact Extractor
+ * 
+ * Extracts all facts necessary for AIDA and puts them in a single file.
+ */
+public class AIDAOneShotExtractor extends SimpleDeduplicator {
+
+  @Override
+  @ImplementationNote("Hardwired facts go first. Infoboxes should go before categories")
+  public List<Theme> inputOrdered() {
+    List<Theme> input = new ArrayList<Theme>();
+    
+    // For YAGO compliance.
+    input.add(SchemaExtractor.YAGOSCHEMA);
+    
+    // Dictionary.
+    input.addAll(StructureExtractor.STRUCTUREFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages)); // also gives links and anchor texts.
+    input.addAll(DisambiguationPageExtractor.DISAMBIGUATIONMEANSFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    input.add(PersonNameExtractor.PERSONNAMES);
+    input.add(PersonNameExtractor.PERSONNAMEHEURISTICS);
+    input.addAll(RedirectExtractor.REDIRECTFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    input.add(GenderExtractor.PERSONS_GENDER);
+    input.add(WikidataLabelExtractor.WIKIPEDIALABELS);
+    input.add(WikidataLabelExtractor.WIKIDATAMULTILABELS);
+    input.add(HardExtractor.HARDWIREDFACTS);
+    
+    // Metadata.
+    input.add(WikiInfoExtractor.WIKIINFO);
+    
+    // Types and Taxonomy.
+    input.add(TransitiveTypeExtractor.TRANSITIVETYPE);
+    input.add(ClassExtractor.YAGOTAXONOMY);
+    
+    // Keyphrases.
+    input.addAll(ConteXtExtractor.CONTEXTFACTS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    input.addAll(CategoryExtractor.CATEGORYMEMBERS.inLanguages(MultilingualExtractor.wikipediaLanguages));
+    return input;
+  }
+
+  /** All facts of YAGO */
+  public static final Theme AIDAFACTS = new Theme("aidaFacts",
+      "All facts necessary for AIDA", ThemeGroup.OTHER);
+
+  /** All facts of YAGO */
+  public static final Theme AIDACONFLICTS = new Theme("_aidaFactConflicts",
+      "Facts that were not added because they conflicted with an existing fact");
+
+  /** Relations that AIDA needs. */
+  public static final Set<String> relations = new FinalSet<>(
+      RDFS.type, RDFS.subclassOf, RDFS.label, "skos:prefLabel",
+      "<isPreferredMeaningOf>", "<hasGivenName>", "<hasFamilyName>",
+      "<hasGender>", "<hasAnchorText>", "<hasInternalWikipediaLinkTo>",
+      "<redirectedFrom>", "<hasWikipediaUrl>", "<hasCitationTitle>",
+      "<hasWikipediaCategory>", "<hasWikipediaAnchorText>");
+
+  @Override
+  public Theme myOutput() {
+    return AIDAFACTS;
+  }
+
+  @Override
+  public Theme conflicts() {
+    return AIDACONFLICTS;
+  }
+
+  @Override
+  public boolean isMyRelation(Fact fact) {
+    if (relations.contains(fact.getRelation())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Announce.setLevel(Announce.Level.DEBUG);
+    new AIDAOneShotExtractor().extract(new File("C:/fabian/data/yago3"), "test");
+  }
+
+}
