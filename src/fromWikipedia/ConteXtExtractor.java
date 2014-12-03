06a0d9b461e2f33fcfe43078fc710aa4cdea2229
mamir <mamir@mpi-inf.mpg.de>
2014-12-03 16:49:30 +0000

    conteXtExtractor needs entity translation followup for multilingual entities to map them to the Enlgish ones when possible

--- src/fromWikipedia/ConteXtExtractor.java
+++ src/fromWikipedia/ConteXtExtractor.java
@@ -19,6 +19,8 @@ import utils.Theme;
 import utils.TitleExtractor;
 import basics.Fact;
 import extractors.MultilingualWikipediaExtractor;
+import followUp.EntityTranslator;
+import followUp.FollowUpExtractor;
 import fromOtherSources.PatternHardExtractor;
 import fromOtherSources.WordnetExtractor;
 
@@ -32,97 +34,105 @@ import fromOtherSources.WordnetExtractor;
  */
 public class ConteXtExtractor extends MultilingualWikipediaExtractor {
 
-	@Override
-	public Set<Theme> input() {
-		return new HashSet<Theme>(Arrays.asList(
-				PatternHardExtractor.CONTEXTPATTERNS,
-				PatternHardExtractor.TITLEPATTERNS,
-				PatternHardExtractor.AIDACLEANINGPATTERNS,
-				WordnetExtractor.PREFMEANINGS,PatternHardExtractor.LANGUAGECODEMAPPING));
-	}
+  @Override
+  public Set<Theme> input() {
+    return new HashSet<Theme>(Arrays.asList(PatternHardExtractor.CONTEXTPATTERNS, PatternHardExtractor.TITLEPATTERNS,
+        PatternHardExtractor.AIDACLEANINGPATTERNS, WordnetExtractor.PREFMEANINGS, PatternHardExtractor.LANGUAGECODEMAPPING));
+  }
 
-	@Override
-	public Set<Theme> inputCached() {
-		return new HashSet<Theme>(Arrays.asList(
-				PatternHardExtractor.CONTEXTPATTERNS,
-				PatternHardExtractor.TITLEPATTERNS,
-        PatternHardExtractor.AIDACLEANINGPATTERNS,
-				WordnetExtractor.PREFMEANINGS,PatternHardExtractor.LANGUAGECODEMAPPING));
-	}
-	 
-	/** Context for entities */
-	public static final MultilingualTheme CONTEXTFACTS = new MultilingualTheme(
-			"yagoConteXtFacts",
-			"Keyphrases for the X in SPOTLX - gathered from (internal and external) link anchors, citations and category names");
+  @Override
+  public Set<Theme> inputCached() {
+    return new HashSet<Theme>(Arrays.asList(PatternHardExtractor.CONTEXTPATTERNS, PatternHardExtractor.TITLEPATTERNS,
+        PatternHardExtractor.AIDACLEANINGPATTERNS, WordnetExtractor.PREFMEANINGS, PatternHardExtractor.LANGUAGECODEMAPPING));
+  }
 
-	@Override
-	public Set<Theme> output() {
-		// return new FinalSet<Theme>(DIRTYCONTEXTFACTS, CONTEXTSOURCES);
-		return new FinalSet<Theme>(CONTEXTFACTS.inLanguage(language));
-	}
+  
+   public static final MultilingualTheme CONTEXTFACTSNEEDSTRANSLATION = new MultilingualTheme(
+        "conteXtFactsNeedsTranslation",
+        "Keyphrases for the X in SPOTLX - gathered from (internal and external) link anchors, citations and category names");
 
-	@Override
-	public void extract() throws Exception {
-		// Extract the information
-		Announce.doing("Extracting context facts");
+   
+  /** Context for entities */
+  public static final MultilingualTheme CONTEXTFACTS = new MultilingualTheme(
+      "yagoConteXtFacts",
+      "Keyphrases for the X in SPOTLX - gathered from (internal and external) link anchors, citations and category names");
 
-		BufferedReader in = FileUtils.getBufferedUTF8Reader(wikipedia);
-		TitleExtractor titleExtractor = new TitleExtractor(language);
+  
+  
+  @Override
+  public Set<Theme> output() {
+    // return new FinalSet<Theme>(DIRTYCONTEXTFACTS, CONTEXTSOURCES);
+    if(isEnglish()) {
+      return new FinalSet<Theme>(CONTEXTFACTS.inLanguage(language));      
+    } else {
+      return new FinalSet<Theme>(CONTEXTFACTSNEEDSTRANSLATION.inLanguage(language));
+    }
+  }
 
-		FactCollection contextPatternCollection = PatternHardExtractor.CONTEXTPATTERNS
-				.factCollection();
-		FactTemplateExtractor contextPatterns = new FactTemplateExtractor(
-				contextPatternCollection, "<_extendedContextWikiPattern>");
-		PatternList replacements = new PatternList(
-        PatternHardExtractor.AIDACLEANINGPATTERNS.factCollection(),
-        "<_aidaCleaning>");
+  @Override
+  public Set<FollowUpExtractor> followUp() {
+    Set<FollowUpExtractor> result = new HashSet<FollowUpExtractor>();
 
-		// FactWriter outSources = output.get(CONTEXTSOURCES);
+    if (!isEnglish()) {
+      result.add(new EntityTranslator(CONTEXTFACTSNEEDSTRANSLATION.inLanguage(language), CONTEXTFACTS.inLanguage(this.language), this));
+    }
+    return result;
+  }
 
-		String titleEntity = null;
-		while (true) {
-			switch (FileLines.findIgnoreCase(in, "<title>")) {
-			case -1:
-				Announce.done();
-				in.close();
-				return;
-			case 0:
-				titleEntity = titleExtractor.getTitleEntity(in);
-				if (titleEntity == null)
-					continue;
+  @Override
+  public void extract() throws Exception {
+    // Extract the information
+    Announce.doing("Extracting context facts");
 
-				String page = FileLines.readBetween(in, "<text", "</text>");
-				String normalizedPage = Char17.decodeAmpersand(Char17
-						.decodeAmpersand(page.replaceAll("[\\s\\x00-\\x1F]+",
-								" ")));
-				String transformedPage = replacements.transform(normalizedPage);
+    BufferedReader in = FileUtils.getBufferedUTF8Reader(wikipedia);
+    TitleExtractor titleExtractor = new TitleExtractor(language);
 
-				// for (Pair<Fact, String> fact :
-				// contextPatterns.extractWithProvenance(normalizedPage,
-				// titleEntity)) {
-				// if (fact.first != null)
-				// write(out, fact.first, outSources,
-				// FactComponent.wikipediaURL(titleEntity),
-				// "ConteXtExtractor from: " + fact.second);
-				// }
-				for (Fact fact : contextPatterns.extract(transformedPage,
-						titleEntity, language)) {
-					if (fact != null) {
-					  CONTEXTFACTS.inLanguage(language).write(fact);
-					}
-				}
-			}
-		}
-	}
+    FactCollection contextPatternCollection = PatternHardExtractor.CONTEXTPATTERNS.factCollection();
+    FactTemplateExtractor contextPatterns = new FactTemplateExtractor(contextPatternCollection, "<_extendedContextWikiPattern>");
+    PatternList replacements = new PatternList(PatternHardExtractor.AIDACLEANINGPATTERNS.factCollection(), "<_aidaCleaning>");
 
-	/**
-	 * Needs Wikipedia as input
-	 * 
-	 * @param wikipedia
-	 *            Wikipedia XML dump
-	 */
-	public ConteXtExtractor(String lang, File wikipedia) {
-		super(lang, wikipedia);
-	}
+    // FactWriter outSources = output.get(CONTEXTSOURCES);
+
+    String titleEntity = null;
+    while (true) {
+      switch (FileLines.findIgnoreCase(in, "<title>")) {
+        case -1:
+          Announce.done();
+          in.close();
+          return;
+        case 0:
+          titleEntity = titleExtractor.getTitleEntity(in);
+          if (titleEntity == null) continue;
+
+          String page = FileLines.readBetween(in, "<text", "</text>");
+          String normalizedPage = Char17.decodeAmpersand(Char17.decodeAmpersand(page.replaceAll("[\\s\\x00-\\x1F]+", " ")));
+          String transformedPage = replacements.transform(normalizedPage);
+
+          // for (Pair<Fact, String> fact :
+          // contextPatterns.extractWithProvenance(normalizedPage,
+          // titleEntity)) {
+          // if (fact.first != null)
+          // write(out, fact.first, outSources,
+          // FactComponent.wikipediaURL(titleEntity),
+          // "ConteXtExtractor from: " + fact.second);
+          // }
+          for (Fact fact : contextPatterns.extract(transformedPage, titleEntity, language)) {
+            if (fact != null) {
+              CONTEXTFACTS.inLanguage(language).write(fact);
+            }
+          }
+      }
+    }
+  }
+
+  /**
+   * Needs Wikipedia as input
+   * 
+   * @param wikipedia
+   *            Wikipedia XML dump
+   */
+  public ConteXtExtractor(String lang, File wikipedia) {
+    super(lang, wikipedia);
+  }
 
 }
