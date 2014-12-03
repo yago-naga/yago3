d95dce95fe425c9296c09d66d06bd5632bc03fb9
mamir <mamir@mpi-inf.mpg.de>
2014-12-03 09:50:38 +0000

    context doesn't need redirecting, type checking!

--- src/fromWikipedia/ConteXtExtractor.java
+++ src/fromWikipedia/ConteXtExtractor.java
@@ -19,10 +19,6 @@ import utils.Theme;
 import utils.TitleExtractor;
 import basics.Fact;
 import extractors.MultilingualWikipediaExtractor;
-import followUp.EntityTranslator;
-import followUp.FollowUpExtractor;
-import followUp.Redirector;
-import followUp.TypeChecker;
 import fromOtherSources.PatternHardExtractor;
 import fromOtherSources.WordnetExtractor;
 
@@ -53,21 +49,6 @@ public class ConteXtExtractor extends MultilingualWikipediaExtractor {
         PatternHardExtractor.INFOBOXPATTERNS,
 				WordnetExtractor.PREFMEANINGS,PatternHardExtractor.LANGUAGECODEMAPPING));
 	}
-	
-	/** Context for entities */
-	public static final MultilingualTheme DIRTYCONTEXTFACTS = new MultilingualTheme(
-			"conteXtFactsDirty",
-			"Keyphrases for the X in SPOTLX - gathered from (internal and external) link anchors, citations and category names - needs redirecting, translation and typechecking");
-
-	public static final MultilingualTheme REDIRECTEDCONTEXTFACTS = new MultilingualTheme(
-			"conteXtFactsRedirected",
-			"Keyphrases for the X in SPOTLX - gathered from (internal and external) link anchors, citations and category names - needs translation and typechecking");
-
-	
-	 public static final MultilingualTheme TRANSLATEDREDIRECTEDCONTEXTFACTS = new MultilingualTheme(
-	      "conteXtFactsTranslatedRedirected",
-	      "Keyphrases for the X in SPOTLX - gathered from (internal and external) link anchors, citations and category names - needs typechecking");
-
 	 
 	/** Context for entities */
 	public static final MultilingualTheme CONTEXTFACTS = new MultilingualTheme(
@@ -77,23 +58,7 @@ public class ConteXtExtractor extends MultilingualWikipediaExtractor {
 	@Override
 	public Set<Theme> output() {
 		// return new FinalSet<Theme>(DIRTYCONTEXTFACTS, CONTEXTSOURCES);
-		return new FinalSet<Theme>(DIRTYCONTEXTFACTS.inLanguage(language));
-	}
-
-	@Override
-	public Set<FollowUpExtractor> followUp() {
-	  Set<FollowUpExtractor> result = new HashSet<FollowUpExtractor>();
-	  
-	   result.add(new Redirector(
-	       DIRTYCONTEXTFACTS.inLanguage(language), REDIRECTEDCONTEXTFACTS.inLanguage(language), this));
-
-	    if (!isEnglish()) {
-	      result.add(new EntityTranslator(REDIRECTEDCONTEXTFACTS.inLanguage(language), TRANSLATEDREDIRECTEDCONTEXTFACTS.inLanguage(this.language), this));
-	      result.add(new TypeChecker(TRANSLATEDREDIRECTEDCONTEXTFACTS.inLanguage(language), CONTEXTFACTS.inLanguage(language), this));
-	    } else {
-	      result.add(new TypeChecker(REDIRECTEDCONTEXTFACTS.inLanguage(language), CONTEXTFACTS.inLanguage(language), this));
-	    }
-		return result;
+		return new FinalSet<Theme>(CONTEXTFACTS.inLanguage(language));
 	}
 
 	@Override
@@ -143,7 +108,7 @@ public class ConteXtExtractor extends MultilingualWikipediaExtractor {
 				for (Fact fact : contextPatterns.extract(transformedPage,
 						titleEntity, language)) {
 					if (fact != null) {
-						DIRTYCONTEXTFACTS.inLanguage(language).write(fact);
+					  CONTEXTFACTS.inLanguage(language).write(fact);
 					}
 				}
 			}
