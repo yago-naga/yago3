e3c62866320664b7d4759e89c8f3962c58352fc4
suchanek <suchanek@mpi-inf.mpg.de>
2015-07-30 17:31:36 +0000

    bug fixes in response to evaluation

--- src/fromThemes/StatisticsExtractor.java
+++ src/fromThemes/StatisticsExtractor.java
@@ -33,83 +33,68 @@ import fromWikipedia.WikiInfoExtractor;
  */
 public class StatisticsExtractor extends Extractor {
 
-	@Override
-	public Set<Theme> input() {
-		return new FinalSet<>(ClassExtractor.YAGOTAXONOMY,
-				CoherentTypeExtractor.YAGOTYPES, FactExtractor.YAGOFACTS,
-				LabelExtractor.YAGOLABELS, MetaFactExtractor.YAGOMETAFACTS,
-				SchemaExtractor.YAGOSCHEMA, DateExtractor.YAGODATEFACTS,
-				LiteralFactExtractor.YAGOLITERALFACTS, WikiInfoExtractor.WIKIINFO);
-	}
+  @Override
+  public Set<Theme> input() {
+    return new FinalSet<Theme>(ClassExtractor.YAGOTAXONOMY, CoherentTypeExtractor.YAGOTYPES, FactExtractor.YAGOFACTS, LabelExtractor.YAGOLABELS,
+        MetaFactExtractor.YAGOMETAFACTS, SchemaExtractor.YAGOSCHEMA, DateExtractor.YAGODATEFACTS, LiteralFactExtractor.YAGOLITERALFACTS,
+        WikiInfoExtractor.WIKIINFO.inLanguage("eng"));
+  }
 
-	/** YAGO statistics theme */
-	public static final Theme STATISTICS = new Theme("yagoStatistics",
-			"Statistics about YAGO and YAGO themes", ThemeGroup.META);
+  /** YAGO statistics theme */
+  public static final Theme STATISTICS = new Theme("yagoStatistics", "Statistics about YAGO and YAGO themes", ThemeGroup.META);
 
-	@Override
-	public Set<Theme> output() {
-		return new FinalSet<>(STATISTICS);
-	}
+  @Override
+  public Set<Theme> output() {
+    return new FinalSet<>(STATISTICS);
+  }
 
-	@Override
-	public void extract() throws Exception {
-		Set<String> definedRelations = new IntHashMap<>();
-		IntHashMap<String> relations = new IntHashMap<>();
-		Set<String> instances = new IntHashMap<>();
-		IntHashMap<String> entityLanguages = new IntHashMap<>();
+  @Override
+  public void extract() throws Exception {
+    Set<String> definedRelations = new IntHashMap<>();
+    IntHashMap<String> relations = new IntHashMap<>();
+    Set<String> instances = new IntHashMap<>();
+    IntHashMap<String> entityLanguages = new IntHashMap<>();
 
-		Announce.doing("Making YAGO statistics");
-		for (Theme t : input()) {
-			Announce.doing("Analyzing", t);
-			int counter = 0;
-			for (Fact f : t) {
-				counter++;
-				if ((f.getRelation().equals(RDFS.domain) || f.getRelation()
-						.equals(RDFS.range))) {
-					definedRelations.add(f.getArg(1));
-				}
-				relations.increase(f.getRelation());
-				if (f.getRelation().equals(RDFS.type)) {
-					instances.add(f.getSubject());
-				}
-			}
-			STATISTICS.write(new Fact(t.asYagoEntity(), YAGO.hasNumber,
-					FactComponent.forNumber(counter)));
-			Announce.done();
-		}
-		for (String rel : relations.keys()) {
-			STATISTICS.write(new Fact(rel.toString(), YAGO.hasNumber,
-					FactComponent.forNumber(relations.get(rel))));
-			if (!definedRelations.contains(rel))
-				Announce.warning("Undefined relation:", rel);
-		}
-		for (String rel : definedRelations) {
-			if (!relations.containsKey(rel))
-				Announce.warning("Unused relation:", rel);
-		}
-		for (String entity : instances) {
-			String lan = FactComponent.getLanguageOfEntity(entity);
-			if (lan != null)
-				entityLanguages.increase(lan);
-		}
-		for (String lan : entityLanguages) {
-			Announce.message(lan, ":", entityLanguages.get(lan), "things");
-			STATISTICS.write(new Fact(FactComponent.forString(lan),
-					FactComponent.forYagoEntity("hasNumberOfThings"),
-					FactComponent.forNumber(entityLanguages.get(lan))));
-		}
-		Announce.message(instances.size(), "things");
-		STATISTICS.write(new Fact(YAGO.yago, FactComponent
-				.forYagoEntity("hasNumberOfThings"), FactComponent
-				.forNumber(instances.size())));
-		STATISTICS.write(new Fact(YAGO.yago, FactComponent
-				.forYagoEntity("wasCreatedOnDate"), FactComponent
-				.forDate(NumberFormatter.ISOdate())));
-		Announce.done();
-	}
+    Announce.doing("Making YAGO statistics");
+    for (Theme t : input()) {
+      Announce.doing("Analyzing", t);
+      int counter = 0;
+      for (Fact f : t) {
+        counter++;
+        if ((f.getRelation().equals(RDFS.domain) || f.getRelation().equals(RDFS.range))) {
+          definedRelations.add(f.getArg(1));
+        }
+        relations.increase(f.getRelation());
+        if (f.getRelation().equals(RDFS.type)) {
+          instances.add(f.getSubject());
+        }
+      }
+      STATISTICS.write(new Fact(t.asYagoEntity(), YAGO.hasNumber, FactComponent.forNumber(counter)));
+      Announce.done();
+    }
+    for (String rel : relations.keys()) {
+      STATISTICS.write(new Fact(rel.toString(), YAGO.hasNumber, FactComponent.forNumber(relations.get(rel))));
+      if (!definedRelations.contains(rel)) Announce.warning("Undefined relation:", rel);
+    }
+    for (String rel : definedRelations) {
+      if (!relations.containsKey(rel)) Announce.warning("Unused relation:", rel);
+    }
+    for (String entity : instances) {
+      String lan = FactComponent.getLanguageOfEntity(entity);
+      if (lan != null) entityLanguages.increase(lan);
+    }
+    for (String lan : entityLanguages) {
+      Announce.message(lan, ":", entityLanguages.get(lan), "things");
+      STATISTICS.write(new Fact(FactComponent.forString(lan), FactComponent.forYagoEntity("hasNumberOfThings"), FactComponent
+          .forNumber(entityLanguages.get(lan))));
+    }
+    Announce.message(instances.size(), "things");
+    STATISTICS.write(new Fact(YAGO.yago, FactComponent.forYagoEntity("hasNumberOfThings"), FactComponent.forNumber(instances.size())));
+    STATISTICS.write(new Fact(YAGO.yago, FactComponent.forYagoEntity("wasCreatedOnDate"), FactComponent.forDate(NumberFormatter.ISOdate())));
+    Announce.done();
+  }
 
-	public static void main(String[] args) throws Exception {
-		new StatisticsExtractor().extract(new File("c:/fabian/data/yago2s"),
-				"test");
-	}
+  public static void main(String[] args) throws Exception {
+    new StatisticsExtractor().extract(new File("c:/fabian/data/yago2s"), "test");
+  }
 }
