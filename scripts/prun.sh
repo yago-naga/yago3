59460fe35570fc54b58bfa785f6d51ade4bd1957
Daniel Bär <dbaer@mpi-inf.mpg.de>
2015-12-17 18:22:20 +0100

    Working on running Yago via Maven

--- scripts/prun.sh
+++ scripts/prun.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-if [ `hostname` != "d5blade09" ]; then
-  echo We should not run this on contact!
-  exit
-fi
+
+# Call it like this: ./prun.sh configuration/my_configuration_file.ini
+
+#if [ `hostname` != "d5blade09" ]; then
+#  echo We should not run this on contact!
+#  exit
+#fi
+
 echo Starting YAGO in parallel, output written to yago.log
-nohup /local/java/bin/java -Xmx44G -cp "../javatools/bin:../basics3/bin:bin" main.ParallelCaller yago.ini > yago.log &
+#nohup /local/java/bin/java -Xmx44G -cp "../javatools/bin:../basics3/bin:bin" main.ParallelCaller yago.ini > yago.log &
+
+export MAVEN_OPTS=-Xmx44G
+mvn exec:java -Dexec.args="$1"
 
 sleep 5s
 tail -f yago.log
