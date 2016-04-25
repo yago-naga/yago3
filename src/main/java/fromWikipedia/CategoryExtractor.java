b81089b7e2f5ae138fc0228b49627df144cb4c21
Fabian M. Suchanek <fabian@suchanek.name>
2016-04-25 16:26:06 +0200

    Added some weird characters that have to be filtered out

--- src/main/java/fromWikipedia/CategoryExtractor.java
+++ src/main/java/fromWikipedia/CategoryExtractor.java
@@ -7,13 +7,8 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
 
-import javatools.datatypes.FinalSet;
-import javatools.filehandlers.FileLines;
-import javatools.util.FileUtils;
-import utils.MultilingualTheme;
-import utils.Theme;
-import utils.TitleExtractor;
 import basics.Fact;
+import basics.Fact.ImplementationNote;
 import basics.FactComponent;
 import extractors.MultilingualWikipediaExtractor;
 import followUp.CategoryTranslator;
@@ -21,6 +16,12 @@ import followUp.FollowUpExtractor;
 import fromOtherSources.DictionaryExtractor;
 import fromOtherSources.PatternHardExtractor;
 import fromOtherSources.WordnetExtractor;
+import javatools.datatypes.FinalSet;
+import javatools.filehandlers.FileLines;
+import javatools.util.FileUtils;
+import utils.MultilingualTheme;
+import utils.Theme;
+import utils.TitleExtractor;
 
 /**
  * CategoryExtractor - YAGO2s
@@ -34,106 +35,88 @@ import fromOtherSources.WordnetExtractor;
 
 public class CategoryExtractor extends MultilingualWikipediaExtractor {
 
-	public static final MultilingualTheme CATEGORYMEMBERS = new MultilingualTheme(
-			"categoryMembers",
-			"Facts about Wikipedia instances, derived from the Wikipedia categories, still to be translated");
+  public static final MultilingualTheme CATEGORYMEMBERS = new MultilingualTheme("categoryMembers",
+      "Facts about Wikipedia instances, derived from the Wikipedia categories, still to be translated");
 
-	public static final MultilingualTheme CATEGORYMEMBERS_TRANSLATED = new MultilingualTheme(
-			"categoryMembersTranslated",
-			"Category Members facts with translated subjects and objects");
+  public static final MultilingualTheme CATEGORYMEMBERS_TRANSLATED = new MultilingualTheme("categoryMembersTranslated",
+      "Category Members facts with translated subjects and objects");
 
-	@Override
-	public Set<Theme> input() {
-		return new TreeSet<Theme>(Arrays.asList(
-				PatternHardExtractor.TITLEPATTERNS,
-				WordnetExtractor.PREFMEANINGS,
-				DictionaryExtractor.CATEGORYWORDS));
-	}
+  @Override
+  public Set<Theme> input() {
+    return new TreeSet<Theme>(Arrays.asList(PatternHardExtractor.TITLEPATTERNS, WordnetExtractor.PREFMEANINGS, DictionaryExtractor.CATEGORYWORDS));
+  }
 
-	@Override
-	public Set<Theme> inputCached() {
-		return new FinalSet<>(WordnetExtractor.PREFMEANINGS,
-				DictionaryExtractor.CATEGORYWORDS);
-	}
+  @Override
+  public Set<Theme> inputCached() {
+    return new FinalSet<>(WordnetExtractor.PREFMEANINGS, DictionaryExtractor.CATEGORYWORDS);
+  }
 
-	@Override
-	public Set<Theme> output() {
-		return new FinalSet<Theme>(CATEGORYMEMBERS.inLanguage(language));
-	}
+  @Override
+  public Set<Theme> output() {
+    return new FinalSet<Theme>(CATEGORYMEMBERS.inLanguage(language));
+  }
 
-	@Override
-	public Set<FollowUpExtractor> followUp() {
-		if (language.equals("en"))
-			return (Collections.emptySet());
-		return (new FinalSet<FollowUpExtractor>(new CategoryTranslator(
-				CATEGORYMEMBERS.inLanguage(this.language),
-				CATEGORYMEMBERS_TRANSLATED.inLanguage(this.language), this)));
-	}
+  @Override
+  public Set<FollowUpExtractor> followUp() {
+    if (language.equals("en")) return (Collections.emptySet());
+    return (new FinalSet<FollowUpExtractor>(
+        new CategoryTranslator(CATEGORYMEMBERS.inLanguage(this.language), CATEGORYMEMBERS_TRANSLATED.inLanguage(this.language), this)));
+  }
 
-	@Override
-	public void extract() throws Exception {
-		TitleExtractor titleExtractor = new TitleExtractor(language);
+  @Override
+  public void extract() throws Exception {
+    TitleExtractor titleExtractor = new TitleExtractor(language);
 
-		// Extract the information
-		// Announce.progressStart("Extracting", 3_900_000);
-		Reader in = FileUtils.getBufferedUTF8Reader(wikipedia);
-		String titleEntity = null;
-		/**
-		 * categoryWord holds the synonym of the word "Category" in different
-		 * languages. It is needed to distinguish the category part in Wiki
-		 * pages.
-		 */
-		String categoryWord = DictionaryExtractor.CATEGORYWORDS
-				.factCollection().getObject(FactComponent.forString(language),
-						"<_hasCategoryWord>");
-		if (categoryWord == null)
-			throw new Exception("Category word undefined in language "
-					+ language);
-		categoryWord = FactComponent.asJavaString(categoryWord);
-		while (true) {
-			switch (FileLines.findIgnoreCase(in, "<title>", "[[Category:", "[["
-					+ categoryWord + ":")) {
-			case -1:
-				// Announce.progressDone();
-				in.close();
-				return;
-			case 0:
-				// Announce.progressStep();
-				titleEntity = titleExtractor.getTitleEntity(in);
-				break;
-			case 1:
-			case 2:
-				if (titleEntity == null) {
-					continue;
-				}
-				String category = FileLines.readTo(in, ']', '|').toString();
-				category = category.trim();
-				// There are sometimes categories of length 0
-				// This causes problems, so avoid them
-				if (category.length() >= 4 && !category.contains(":")) {
-					CATEGORYMEMBERS.inLanguage(language).write(
-							new Fact(titleEntity, "<hasWikipediaCategory>",
-									FactComponent.forForeignWikiCategory(category,
-											language)));
-				}
-				break;
-			}
-		}
-	}
+    // Extract the information
+    // Announce.progressStart("Extracting", 3_900_000);
+    Reader in = FileUtils.getBufferedUTF8Reader(wikipedia);
+    String titleEntity = null;
+    /**
+     * categoryWord holds the synonym of the word "Category" in different
+     * languages. It is needed to distinguish the category part in Wiki
+     * pages.
+     */
+    String categoryWord = DictionaryExtractor.CATEGORYWORDS.factCollection().getObject(FactComponent.forString(language), "<_hasCategoryWord>");
+    if (categoryWord == null) throw new Exception("Category word undefined in language " + language);
+    categoryWord = FactComponent.asJavaString(categoryWord);
+    while (true) {
+      switch (FileLines.findIgnoreCase(in, "<title>", "[[Category:", "[[" + categoryWord + ":")) {
+        case -1:
+          // Announce.progressDone();
+          in.close();
+          return;
+        case 0:
+          // Announce.progressStep();
+          titleEntity = titleExtractor.getTitleEntity(in);
+          break;
+        case 1:
+        case 2:
+          if (titleEntity == null) {
+            continue;
+          }
+          @ImplementationNote("All of these weird characters can erroneously appear in category names. We cut them away.")
+          String category = FileLines.readTo(in, ']', '|', '{', '}').toString();
+          category = category.trim();
+          // There are sometimes categories of length 0
+          // This causes problems, so avoid them
+          if (category.length() >= 4 && !category.contains(":")) {
+            CATEGORYMEMBERS.inLanguage(language)
+                .write(new Fact(titleEntity, "<hasWikipediaCategory>", FactComponent.forForeignWikiCategory(category, language)));
+          }
+          break;
+      }
+    }
+  }
 
-	public CategoryExtractor(String lang, File wikipedia) {
-		super(lang, wikipedia);
-	}
+  public CategoryExtractor(String lang, File wikipedia) {
+    super(lang, wikipedia);
+  }
 
-	public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-		new CategoryExtractor(
-				"de",
-				new File(
-						"C:/Fabian/eclipseProjects/yago3/testCases/fromWikipedia.CategoryExtractor/de/wikipedia/de_wikitest.xml"))
-				.extract(new File("c:/fabian/data/yago3"),
-						"Test on 1 wikipedia article");
+    new CategoryExtractor("de", new File("C:/Fabian/eclipseProjects/yago3/testCases/fromWikipedia.CategoryExtractor/de/wikipedia/de_wikitest.xml"))
+        .extract(new File("c:/fabian/data/yago3"), "Test on 1 wikipedia article");
 
-	}
+  }
 
 }
