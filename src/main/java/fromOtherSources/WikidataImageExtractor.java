0696c6feb0b7f67f27624d7c3e59c6431fdf2fca
Ghazaleh Haratinezhad Torbati <ghazaleh@mpi-inf.mpg.de>
2017-01-13 19:15:27 +0100

    Unit test for wikidataImageExtractor and fix a bug in wikidataImageExtractor

--- src/main/java/fromOtherSources/WikidataImageExtractor.java
+++ src/main/java/fromOtherSources/WikidataImageExtractor.java
@@ -1,9 +1,7 @@
 package fromOtherSources;
 
-import java.io.File; 
+import java.io.File;  
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -23,6 +21,7 @@ import fromThemes.TransitiveTypeExtractor;
 import javatools.administrative.Parameters;
 import javatools.datatypes.FinalSet;
 import javatools.parsers.Char17;
+import utils.FactCollection;
 import utils.Theme;
 
 /**
@@ -56,6 +55,9 @@ public class WikidataImageExtractor extends DataExtractor {
 	private static final String WIKIDATA_STATEMENTS = "wikidata_statements";
 	private static final String IMAGE_ORIGINALURL_TEMPLATE = "https://upload.wikimedia.org/wikipedia/commons/";
 	private static final String IMAGETYPE = "image_";
+	private static FactCollection transitiveTypes = new FactCollection();
+	private static FactCollection reverseWikidataInstances = new FactCollection();
+	
 	
 	private static final Map<String, List<String>> imageRelations;
 	static {
@@ -91,68 +93,131 @@ public class WikidataImageExtractor extends DataExtractor {
 
 	@Override
 	public void extract() throws Exception {
-		Map<String, String> wikiDataIdEntityMap = WikidataLabelExtractor.WIKIDATAINSTANCES.factCollection().getReverseMap(RDFS.sameas);
+	  // Example of the facts in reverseWikidataInstances:
+	  // <http://www.wikidata.org/entity/Q23>  owl:sameAs <George_Washington>      
+	  reverseWikidataInstances = WikidataLabelExtractor.WIKIDATAINSTANCES.factCollection().getReverse();
+		
+	  transitiveTypes = TransitiveTypeExtractor.TRANSITIVETYPE.factCollection();
+		
 		N4Reader nr = new N4Reader(inputData);
-		String yagoEntity = null;
+		String yagoEntityMostEnglish = null;
 		Fact prevImage = null;
 		int imageCounter = 1;
 		Map<String, String> images = new HashMap<String, String>();
 		while(nr.hasNext()) {
 			Fact f = nr.next();
-			// example: <http://www.wikidata.org/entity/Q1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.wikidata.org/ontology#Item> 
+			 
 			// We reached a new entity and until the next appearance of "#Item", the statements are about this entity.
+			// example: <http://www.wikidata.org/entity/Q1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.wikidata.org/ontology#Item>
 			if(f.getObject().endsWith("#Item>")){
 				if(!images.isEmpty()){
-					String image = null;
-					String category = getHighlevelCategory(yagoEntity);
-					for(String key : imageRelations.get(category)){
-						if(images.containsKey(key)){
-							image = images.get(key);
-							break;
-						}
-					}
-					// If there were some image(s) but not in the expected types, select first one.
-					if(image == null){
-						image = images.entrySet().iterator().next().getValue();
-					}
+					String image = pickImage(yagoEntityMostEnglish, images);
 					
-					  
-					String originalUrl = FactComponent.forUri(getOriginalImageUrl(FactComponent.stripBrackets(image), imageCounter));
-					String imageID = IMAGETYPE + imageCounter++;
+					// Get original image url from image's wikipage url
+					String originalUrl = FactComponent.forUri(getOriginalImageUrl(FactComponent.stripBrackets(image)));
+					String imageID = FactComponent.forYagoEntity(IMAGETYPE + imageCounter);
+					imageCounter++;
 					
-					
-					WIKIDATAIMAGES.write(new Fact(yagoEntity, YAGO.hasWikiDataImage, imageID));
+					// Saving imageID along with image wiki page url and image original url to the theme
+					WIKIDATAIMAGES.write(new Fact(yagoEntityMostEnglish, YAGO.hasWikiDataImage, imageID));
 					WIKIDATAIMAGES.write(new Fact(imageID, YAGO.hasWikiPage, image));
 					WIKIDATAIMAGES.write(new Fact(imageID, YAGO.hasImageUrl, originalUrl));
+	        images.clear();
 				}
-				images.clear();
-				yagoEntity = wikiDataIdEntityMap.get(f.getSubject());
+				// reverseWikidataInstances facts like: <http://www.wikidata.org/entity/Q23>  owl:sameAs <George_Washington>
+				yagoEntityMostEnglish = getMostEnglishEntityName(reverseWikidataInstances.getFactsWithSubjectAndRelation(f.getSubject(), RDFS.sameas));
 				prevImage = null;
 			}
+			
 			// Select the first image per relation for an entity, unless there is a PreferredRank.
 			// Format of regular expression is taken from: https://www.wikidata.org/wiki/Property:P18
-			else if(f.getObject().matches(".*commons\\.wikimedia.*\\.(jpg|jpeg|png|svg|tif|tiff|gif)>$") && yagoEntity != null){
+			// example: <some random id> <https://www.wikidata.org/wiki/Property:P18> <filename.jpg> .
+			else if(f.getObject().matches(".*commons\\.wikimedia.*\\.(jpg|jpeg|png|svg|tif|tiff|gif)>$") && yagoEntityMostEnglish != null){
 				if(!images.containsKey(f.getRelation()))
 					images.put(f.getRelation(), Char17.decodeBackslash(f.getObject()));
 				prevImage = f;
 			}
 			// If the rank of the image was "PreferredRank", replace the existing image with this preferred image.
+			// example : <random id> <http://www.wikidata.org/ontology#rank> <http://www.wikidata.org/ontology#NormalRank> . 
 			else if(prevImage != null && prevImage.getSubject().equals(f.getSubject()) && f.getRelation().endsWith("#rank>")){
 				if(f.getObject().endsWith("#PreferredRank>"))
 					images.put(prevImage.getRelation(), prevImage.getObject());
 				prevImage = null;
 			}
 		}
+		
+    // Saving information of the last entity in the file
+		if (!images.isEmpty() && yagoEntityMostEnglish != null)
+		{
+		  String image = pickImage(yagoEntityMostEnglish, images);
+		  String originalUrl = FactComponent.forUri(getOriginalImageUrl(FactComponent.stripBrackets(image)));
+		  String imageID = FactComponent.forYagoEntity(IMAGETYPE + imageCounter);
+		  
+      WIKIDATAIMAGES.write(new Fact(yagoEntityMostEnglish, YAGO.hasWikiDataImage, imageID));
+      WIKIDATAIMAGES.write(new Fact(imageID, YAGO.hasWikiPage, image));
+      WIKIDATAIMAGES.write(new Fact(imageID, YAGO.hasImageUrl, originalUrl));
+      images.clear();
+		}
+		
 		nr.close();
 	}
 	
-  /*
-   * To make the image's original url, we use the first 2 characters of md5 hashed version of the file name
+	/**
+	 * Return the most English entity name given all entity names available
+	 * @param entityFacts 
+	 * @return
+	 */
+	private static String getMostEnglishEntityName(Set<Fact> entityFacts){
+	  // Map of entity names for each language 
+    Map<String, String> languageEntityName = new HashMap<>();
+    // each entityFact is like: <http://www.wikidata.org/entity/Q23>  owl:sameAs <George_Washington>
+    for(Fact f:entityFacts){
+      String language = FactComponent.getLanguageOfEntity(f.getObject());
+      if (language != null)
+        languageEntityName.put(language, f.getObject());
+      else
+        languageEntityName.put("en", f.getObject());
+    }
+    
+    String mostEnglishLanguage = DictionaryExtractor.mostEnglishLanguage(languageEntityName.keySet());
+    return languageEntityName.get(mostEnglishLanguage);
+	}
+	
+	
+	/**
+	 * Pick the best image with regard to its category using manual order for images for each category.
+	 * @param yagoEntity
+	 * @param images
+	 * @return image
+	 * @throws IOException
+	 */
+	private static String pickImage(String yagoEntity, Map<String, String> images) throws IOException {
+	  String image = null;
+	  String category = getHighlevelCategory(yagoEntity);
+    for(String key : imageRelations.get(category)){
+      if(images.containsKey(key)){
+        image = images.get(key);
+        break;
+      }
+    }
+    // If there were some image(s) but not in the expected types, select first one.
+    if(image == null){
+      image = images.entrySet().iterator().next().getValue();
+    }
+	  return image;
+	}
+	
+	
+	/**
+	 * To make the image's original url, we use the first 2 characters of md5 hashed version of the file name
    * example: input= "https://commons.wikimedia.org/wiki/File:Spelterini_Blüemlisalp.jpg" 
    * file name = "Spelterini_Blüemlisalp.jpg" hashed = "ae1a26d34d6a674d4400c8a1e6fe73f8"
    * original url = https://upload.wikimedia.org/wikipedia/commons/a/ae/Spelterini_Bl%C3%BCemlisalp.jpg 
-   */
-	private String getOriginalImageUrl(String wikiUrl, int cnt) throws NoSuchAlgorithmException {
+	 * @param wikiUrl
+	 * @return originalUrl
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static String getOriginalImageUrl(String wikiUrl) throws NoSuchAlgorithmException {
 	  MessageDigest md = MessageDigest.getInstance("MD5");
 	  String imageName = wikiUrl.substring(wikiUrl.indexOf("/wiki/File:") + "/wiki/File:".length());
 	  StringBuffer hashedName = new StringBuffer();
@@ -165,8 +230,14 @@ public class WikidataImageExtractor extends DataExtractor {
     return (IMAGE_ORIGINALURL_TEMPLATE + hashedName.charAt(0) + "/" + hashedName.charAt(0) + hashedName.charAt(1) + "/" + imageName);
 	}
 	
-	private String getHighlevelCategory(String entity) throws IOException {
-		Set<Fact> facts = TransitiveTypeExtractor.TRANSITIVETYPE.factCollection().getFactsWithSubjectAndRelation(entity, RDFS.type);
+	/**
+	 * Return the high level category of the entity based on yago transitive types
+	 * @param entity
+	 * @return category
+	 * @throws IOException
+	 */
+	private static String getHighlevelCategory(String entity) throws IOException {
+		Set<Fact> facts = transitiveTypes.getFactsWithSubjectAndRelation(entity, RDFS.type);
 		String category = "other";
 		
 		for (Fact fact:facts){
@@ -192,7 +263,6 @@ public class WikidataImageExtractor extends DataExtractor {
 				break;
 			}		
 		}
-		
 		return category;
 	}
 
---
+++ src/test/java/fromOtherSources/WikidataImageExtractorTest.java
@@ -0,0 +1,99 @@
+package fromOtherSources;
+
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import extractors.MultilingualExtractor;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test cases for WikidataImageExtractor
+ * @author Ghazaleh Haratinezhad
+ *
+ */
+
+public class WikidataImageExtractorTest {
+  
+  private void testGetOriginalImageUrl() throws NoSuchAlgorithmException {
+    
+    List <String> inputImageWikiUrls = Arrays.asList(
+        "http://commons.wikimedia.org/wiki/File:Breviarium_Grimani_-_November.jpg",
+        "http://commons.wikimedia.org/wiki/File:Flag_of_Moldova.svg",
+        "http://commons.wikimedia.org/wiki/File:POL_Kurów_COA.svg",
+        "http://commons.wikimedia.org/wiki/File:Champs-Élysées_from_the_Arc_de_Triomphe.jpg",
+        "http://commons.wikimedia.org/wiki/File:Corallushortulanus.png",
+        "http://commons.wikimedia.org/wiki/File:Aurélien_Clerc.jpg",
+        "http://commons.wikimedia.org/wiki/File:Nyiragongo_1994.jpg",
+        "http://commons.wikimedia.org/wiki/File:Torre_Belém_April_2009-4a.jpg",
+        "http://commons.wikimedia.org/wiki/File:De_fem_søstre_i_Århus_final_version.jpg",
+        "http://commons.wikimedia.org/wiki/File:Network_of_the_Presidents_of_the_Supreme_Judicial_Courts_of_the_European_Union_Logo.png");
+    
+    List<String> outputOriginalUrls = Arrays.asList(
+        "https://upload.wikimedia.org/wikipedia/commons/3/35/Breviarium_Grimani_-_November.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/2/27/Flag_of_Moldova.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/d/d4/POL_Kurów_COA.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/9/95/Champs-Élysées_from_the_Arc_de_Triomphe.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/2/2b/Corallushortulanus.png",
+        "https://upload.wikimedia.org/wikipedia/commons/f/f7/Aurélien_Clerc.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/6/68/Nyiragongo_1994.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/6/65/Torre_Belém_April_2009-4a.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/2/23/De_fem_søstre_i_Århus_final_version.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/2/2e/Network_of_the_Presidents_of_the_Supreme_Judicial_Courts_of_the_European_Union_Logo.png");
+    
+    List<String> testOutputOriginalUrls = new LinkedList<>();    
+    
+    for(String url:inputImageWikiUrls) {
+      testOutputOriginalUrls.add(WikidataImageExtractor.getOriginalImageUrl(url));
+    }
+    
+    assertEquals(outputOriginalUrls.size(), testOutputOriginalUrls.size());
+    assertArrayEquals(outputOriginalUrls.toArray(), testOutputOriginalUrls.toArray());
+    
+  }
+  
+  public void compareFiles(String path) throws IOException{
+    String actual   = new String(Files.readAllBytes(Paths.get(path+"wikidataImages.tsv")));
+    String expected = new String(Files.readAllBytes(Paths.get(path+"Expected_wikidataImages.tsv")));
+    
+    assertEquals(actual, expected);
+  }
+  
+  @Test
+  public void test() {
+    
+    // Test getOriginalImageUrl function:
+    try {
+      testGetOriginalImageUrl();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    
+    // Set the languages to de and en:
+    MultilingualExtractor.wikipediaLanguages = Arrays.asList("en", "de");
+    
+    // Test Extraction on small input:
+    WikidataImageExtractor ex = new WikidataImageExtractor(new File("src/test/resources/fromOtherSources.WikidataImageExtractor/input/sample-wikidata-statements.nt"));
+    try {
+      // The first argument should be the folder containing the themes needed in the extractor.
+      // WikidataImageExtractor needs following files: "wikidataInstances.tsv" and "yagoTransitiveType.tsv"
+      // I made small files for this test and save them in resources. But original files are also usable.
+      // Output of the extract function (wikidataImages.tsv) is written in the second second argument.
+      ex.extract(new File("src/test/resources/fromOtherSources.WikidataImageExtractor/input"), new File("src/test/resources/fromOtherSources.WikidataImageExtractor/output"),"testing WikidataImageExtractor");
+      // Compare output file with the expected file
+      compareFiles("src/test/resources/fromOtherSources.WikidataImageExtractor/output/");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+  }
+
+}
