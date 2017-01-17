6db7dc11c21702b322dcfdc98bdbf085862fc061
Daniel Bär <daniel@ambiverse.com>
2017-01-17 10:17:52 +0100

    Added UTF-8 encoding parameter

--- src/main/java/fromOtherSources/WikidataImageExtractor.java
+++ src/main/java/fromOtherSources/WikidataImageExtractor.java
@@ -2,6 +2,7 @@ package fromOtherSources;
 
 import java.io.File;  
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -219,10 +220,13 @@ public class WikidataImageExtractor extends DataExtractor {
 	 */
 	public static String getOriginalImageUrl(String wikiUrl) throws NoSuchAlgorithmException {
 	  MessageDigest md = MessageDigest.getInstance("MD5");
+	  
 	  String imageName = wikiUrl.substring(wikiUrl.indexOf("/wiki/File:") + "/wiki/File:".length());
+	  
 	  StringBuffer hashedName = new StringBuffer();
-	  md.update(imageName.getBytes());
+	  md.update(imageName.getBytes(StandardCharsets.UTF_8));
 	  byte byteData[] = md.digest();
+	  
 	  for (int i = 0; i < byteData.length; i++) {
 	    hashedName.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
 	  }
