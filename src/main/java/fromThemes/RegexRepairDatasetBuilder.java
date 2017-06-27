4cf975c0783c0c3da74d31e2f4abad05bff2d85c
Thomas Rebele <thomas.rebele@googlemail.com>
2017-06-27 23:34:09 +0200

    create output files

--- src/main/java/fromThemes/RegexRepairDatasetBuilder.java
+++ src/main/java/fromThemes/RegexRepairDatasetBuilder.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import basics.Fact;
 import basics.FactComponent;
@@ -48,6 +48,10 @@ public class RegexRepairDatasetBuilder extends InfoboxTermExtractor {
     DateParser dateParser = new DateParser();
     NumberParser numberParser = new NumberParser();
 
+    Pattern datePattern = Pattern.compile("_result_\\d+-\\d+-\\d+_");
+    Pattern numberPattern = Pattern.compile("_result_[^_]*_<[^>%]*>_");
+    Pattern numberPatternExclude = Pattern.compile("<percent>");
+
     try (Writer datesOut = java.nio.file.Files.newBufferedWriter(dateOutputFile.toPath())) {
       try (Writer numbersOut = java.nio.file.Files.newBufferedWriter(numberOutputFile.toPath())) {
         for (Fact f : InfoboxExtractor.INFOBOX_ATTRIBUTES.inLanguage(this.language)) {
@@ -59,28 +63,49 @@ public class RegexRepairDatasetBuilder extends InfoboxTermExtractor {
           val = val.replace("$0", FactComponent.stripBrackets(f.getSubject()));
           val = val.trim();
           if (val.length() == 0) continue;
-          List<Integer> startIdx = new ArrayList<>(), endIdx = new ArrayList<>();
-          String transformed = dateParser.patternList.transformWithProvenance(val, startIdx, endIdx);
-          if (!Objects.equals(val, transformed)) {
-            System.out.println(val);
-            System.out.println(transformed);
-          }
-          Matcher m = DateParser.resultPattern.matcher(transformed);
-          if (m.find()) {
-            do {
-              int start = Collections.min(startIdx.subList(m.start(), m.end()));
-              int end = Collections.max(endIdx.subList(m.start(), m.end()));
-              System.out.println("======= DATE: '" + val.substring(start, end) + "'");
-            } while (m.find());
 
-            //datesOut.write(m.replaceAll("<date>$1</date>") + "\n");
+          String newVal = mark(val, dateParser.patternList, datePattern, null, "date");
+          if (newVal != null) {
+            datesOut.write("\t" + f.getSubject() + "\t" + f.getRelation() + "\t" + FactComponent.forString(newVal) + "\n");
           }
-          transformed = numberParser.patternList.transform(val);
-          m = NumberParser.resultPattern.matcher(transformed);
-          if (m.find()) numbersOut.write(m.replaceAll("<number>$1</number>") + "\n");
+          newVal = mark(val, numberParser.patternList, numberPattern, numberPatternExclude, "number");
+          if (newVal != null) {
+            numbersOut.write("\t" + f.getSubject() + "\t" + f.getRelation() + "\t" + FactComponent.forString(newVal) + "\n");
+          }
         }
       }
     }
+  }
+
+  protected String mark(String val, PatternList patternList, Pattern pattern, Pattern exclude, String tag) {
+    List<Integer> startIdx = new ArrayList<>(), endIdx = new ArrayList<>();
+    String transformed = patternList.transformWithProvenance(val, startIdx, endIdx);
+    Matcher m = pattern.matcher(transformed);
+    if (m.find()) {
+      StringBuffer sb = new StringBuffer();
+      int lastValPos = 0;
+      do {
+        System.out.println("found " + m.group());
+        if (exclude != null && exclude.matcher(m.group()).find()) {
+          continue;
+        }
+        int start = Collections.min(startIdx.subList(m.start(), m.end()));
+        int end = Collections.max(endIdx.subList(m.start(), m.end()));
+        sb.append(val.substring(lastValPos, start));
+        sb.append("<").append(tag).append(">");
+        sb.append(val.substring(start, end));
+        sb.append("</").append(tag).append(">");
+        lastValPos = end;
+      } while (m.find());
+      sb.append(val.substring(lastValPos));
+
+      if (lastValPos != 0) {
+        System.out.println("=====" + tag.toUpperCase() + ":" + sb.toString());
+        return sb.toString();
+      }
+      //datesOut.write(m.replaceAll("<date>$1</date>") + "\n");
+    }
+    return null;
   }
 
   public RegexRepairDatasetBuilder(String lang) {
